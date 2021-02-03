4243bfd09c9281a6a76a35bfcb10e7668b225b05
arseniy <arsen39@gmail.com>
2021-02-03 20:03:13 +0200

    Add Fetch

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,20 @@
 const container = document.getElementById("cards-container");
 
-const filtredDB = db.filter(
-  (person) =>
-    person.firstName !== "" && person.lastName !== "" && person.id !== ""
-);
+fetch('http://192.168.1.148:3000/users')
+.then((res)=>res.json())
+.then((data)=>pageAppender(data))
 
-const cards = filtredDB.map((person) => createPersonCard(person));
-container.append(...cards);
+
+function pageAppender (data) {
+  const filtredDB = data.filter(
+    (person) =>
+      person.firstName !== "" && person.lastName !== "" && person.id !== ""
+  );
+  
+  const cards = filtredDB.map((person) => createPersonCard(person));
+  container.append(...cards);
+}
+
+
 
 
